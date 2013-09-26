994216a78097031f1bc2deb64c2898f86a82c611
David Christian <david@dchristian.co.uk>
2013-09-26 21:56:18 +0100

    Fixed code to be PEP8 friendly
    Made code more PEP8 friendly and simplified kernel boundary checking
    using comparison chaining.

--- stereomatch_SSD.py
+++ stereomatch_SSD.py
@@ -30,81 +30,83 @@ import numpy as np
 import matplotlib.pyplot as plt
 import PIL
 
+
 def stereo_match(left_img, right_img):
     # Load in both images. These are assumed to be RGBA format images
     left_img = PIL.Image.open(left_img)
-    left = np.asarray(left_img) 
+    left = np.asarray(left_img)
     right_img = PIL.Image.open(right_img)
-    right = np.asarray(right_img)  
+    right = np.asarray(right_img)
 
     # Initial SSD
-    w,h = left_img.size # Assumes that both images are same size
-    ssd = np.empty((w,h), np.uint8)
-    ssd.shape = h,w
+    w, h = left_img.size  # Assumes that both images are same size
+    ssd = np.empty((w, h), np.uint8)
+    ssd.shape = h, w
 
     # SSD support window (kernel)
-    win_ssd = np.empty((w,h), np.uint16)
-    win_ssd.shape = h,w
-        
+    win_ssd = np.empty((w, h), np.uint16)
+    win_ssd.shape = h, w
+
     # Depth (or disparity) map
-    depth = np.empty((w,h), np.uint8)
-    depth.shape = h,w
+    depth = np.empty((w, h), np.uint8)
+    depth.shape = h, w
 
     # Minimum ssd difference between both images
-    min_ssd = np.empty((w,h), np.uint16)
-    min_ssd.shape = h,w
+    min_ssd = np.empty((w, h), np.uint16)
+    min_ssd.shape = h, w
     for y in xrange(h):
         for x in xrange(w):
-            min_ssd[y, x] = 65535 # Init to high value
-            
+            min_ssd[y, x] = 65535  # Init to high value
+
     max_offset = 30
-    offset_adjust = 255 / max_offset # used to brighten depth map
+    offset_adjust = 255 / max_offset  # used to brighten depth map
 
     # Create ranges now instead of per loop
     y_range = xrange(h)
     x_range = xrange(w)
-    x_range_ssd = xrange(w) 
-    
+    x_range_ssd = xrange(w)
+
     # u and v support window (currently -4 to 4, so 8x8)
-    window_range = xrange(-4, 5) 
+    window_range = xrange(-4, 5)
 
     # Main loop....
-    for offset in xrange(max_offset):               
-        # Create initial ssd image
+    for offset in xrange(max_offset):
+    # Create initial ssd image
         for y in y_range:
             for x in x_range_ssd:
-                if (x - offset > 0):
-                    diff = left[y, x, 0] - right[y, x - offset, 0]                                                            
-                    ssd[y, x] = diff * diff # Sum squared diffs
+                if x - offset > 0:
+                    diff = left[y, x, 0] - right[y, x - offset, 0]
+                    ssd[y, x] = diff * diff  # Sum squared diffs
 
         # Create a sum of abs differences at this offset    
         for y in y_range:
             for x in x_range:
-                sum_sad = 0                
+                sum_sad = 0
                 # Calculate the sum of squared differences window
                 for i in window_range:
                     for j in window_range:
-                        if (y + i > -1 and y + i < h and x + j > -1 and x + j < w):
-                            sum_sad += ssd[y + i, x + j]                   
-				
-                # Store the sum in the windows SSD image
-                win_ssd[y, x] = sum_sad 
+                        # TODO: I need to replace this by surrounding image with buffer
+                        if (-1 < y + i < h) and (-1 < x + j < w):
+                            sum_sad += ssd[y + i, x + j]
 
-        # Update the min abs diff image with this new data
+                            # Store the sum in the windows SSD image
+                win_ssd[y, x] = sum_sad
+
+                # Update the min abs diff image with this new data
         for y in y_range:
             for x in x_range:
                 # Is this new windowed SSD pixel a better match?
                 if win_ssd[y, x] < min_ssd[y, x]:
                     # If so, store it and add to the depth map      
-                    min_ssd[y, x] = win_ssd[y, x]                
+                    min_ssd[y, x] = win_ssd[y, x]
                     depth[y, x] = offset * offset_adjust
 
         print "Calculated offset ", offset
-        
+
     # Convert to PIL and save it
-    pil_img = PIL.Image.fromarray(depth).save('depth.png')
+    PIL.Image.fromarray(depth).save('depth.png')
 
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     stereo_match("bowling_small_l.png", "bowling_small_r.png")
 
